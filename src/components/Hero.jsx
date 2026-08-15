d388a25bc31c63e6d3b4e4119f9a73709a7fdd7a
taufiq <fiqrmdn@gmail.com>
2026-08-15 21:33:28 +0700

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,52 +22,146 @@ export default function Hero() {
         overflow-hidden
       "
     >
-      <div className="relative z-10 flex flex-col items-center">
+      {/* =========================
+          BUNGA SUDUT KIRI ATAS
+      ========================== */}
+      <img
+        src="/images/hero-flower1.png"
+        alt=""
+        className="
+          absolute
+          top-0
+          left-0
+          w-28
+          sm:w-40
+          md:w-52
+          lg:w-60
+          h-auto
+          pointer-events-none
+          select-none
+          z-0
+        "
+      />
 
-        <p
+      {/* =========================
+          BUNGA SUDUT KANAN ATAS
+      ========================== */}
+      <img
+        src="/images/hero-flower2.png"
+        alt=""
+        className="
+          absolute
+          top-0
+          right-0
+          w-28
+          sm:w-40
+          md:w-52
+          lg:w-60
+          h-auto
+          scale-x-[-1]
+          pointer-events-none
+          select-none
+          z-0
+        "
+      />
+
+      {/* =========================
+          LINGKARAN BUNGA
+      ========================== */}
+      <div
+        className="
+          relative
+          z-10
+          w-[380px]
+          h-[380px]
+          sm:w-[460px]
+          sm:h-[460px]
+          md:w-[540px]
+          md:h-[540px]
+          lg:w-[600px]
+          lg:h-[600px]
+          flex
+          items-center
+          justify-center
+        "
+      >
+        {/* Gambar bunga berbentuk lingkaran */}
+        <img
+          src="/images/flower-circle.png"
+          alt=""
           className="
-            font-serif
-            text-xl
-            sm:text-2xl
-            md:text-3xl
-            tracking-[0.25em]
-            text-red-400
+            absolute
+            inset-0
+            w-full
+            h-full
+            object-contain
+            pointer-events-none
+            select-none
+          "
+        />
+
+        {/* =========================
+            TULISAN DI TENGAH
+        ========================== */}
+        <div
+          className="
+            relative
+            z-10
+            flex
+            flex-col
+            items-center
+            justify-center
           "
         >
-          Happy
-        </p>
+          <p
+            className="
+              font-serif
+              text-lg
+              sm:text-xl
+              md:text-2xl
+              lg:text-3xl
+              tracking-[0.25em]
+              text-white
+            "
+          >
+            Happy
+          </p>
 
-        <h1
-          className="
-            font-title
-            mt-2
-            text-6xl
-            sm:text-7xl
-            md:text-8xl
-            lg:text-9xl
-            leading-none
-            text-red-400
-          "
-        >
-          Birthday
-        </h1>
+          <h1
+            className="
+              font-title
+              mt-2
+              text-5xl
+              sm:text-6xl
+              md:text-7xl
+              lg:text-8xl
+              leading-none
+              text-white
+            "
+          >
+            Birthday
+          </h1>
 
-        <p
-          className="
-            font-serif
-            mt-5
-            text-2xl
-            sm:text-3xl
-            md:text-4xl
-            tracking-wide
-            text-white
-          "
-        >
-          Topik
-        </p>
-
+          <p
+            className="
+              font-serif
+              mt-4
+              text-xl
+              sm:text-2xl
+              md:text-3xl
+              lg:text-4xl
+              tracking-wide
+              text-white
+            "
+          >
+            Topik
+          </p>
+        </div>
       </div>
 
+      {/* =========================
+          TOMBOL SCROLL
+      ========================== */}
       <button
         type="button"
         onClick={scrollToTimeline}
@@ -77,7 +171,7 @@ export default function Hero() {
           sm:bottom-10
           left-1/2
           -translate-x-1/2
-          z-10
+          z-20
           font-text
           text-sm
           sm:text-base
