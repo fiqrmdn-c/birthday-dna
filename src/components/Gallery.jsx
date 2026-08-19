37e1b02276f1b52313ebeb928c0f39a592372ef6
taufiq <fiqrmdn@gmail.com>
2026-08-19 22:45:37 +0700

    update

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -10,157 +10,49 @@ const photos = [
 
 export default function Gallery() {
   return (
-    <section className="min-h-screen px-3 py-10 sm:px-6 sm:py-16">
+    <section className="relative w-full px-0 py-10 sm:py-16">
+      <div className="mx-auto w-full max-w-[1024px]">
 
-      <div className="max-w-6xl mx-auto">
+        {/* =========================================
+            CANVAS GALLERY
+            RASIO MENGIKUTI REFERENSI 1024 : 1448
+        ========================================= */}
 
-        <div className="relative h-[680px] sm:h-[800px] md:h-[900px]">
-
+        <div
+          className="
+            relative
+            w-full
+            aspect-[1024/1448]
+          "
+        >
 
           {/* =========================================
               FOTO 1
-              DIAGONAL PANJANG
-              DARI BAWAH TENGAH KE KIRI ATAS
+              KIRI ATAS
+              FOTO PANJANG VERTIKAL
           ========================================= */}
 
           <div
             className="
               absolute
-              left-[20%]
-              bottom-[5%]
-              w-[120px]
-              sm:left-[10%]
-              sm:bottom-[6%]
-              sm:w-[155px]
-              md:left-[12%]
-              md:bottom-[5%]
-              md:w-[190px]
-              lg:w-[220px]
-              rotate-[-25deg]
-              z-30
+              z-20
+
+              left-[12.5%]
+              top-[12.5%]
+
+              w-[27%]
+
+              rotate-[-11deg]
             "
           >
             <img
               src={photos[0]}
-              alt="Foto diagonal"
+              alt="Foto 1"
               className="
+                block
                 w-full
-                h-[340px]
-                sm:h-[460px]
-                md:h-[580px]
-                lg:h-[680px]
-                object-cover
-              "
-              draggable={false}
-            />
-          </div>
-
-
-          {/* =========================================
-              FOTO 2
-              FOTO SEDANG DI KIRI TENGAH
-          ========================================= */}
-
-          <div
-            className="
-              absolute
-              left-[4%]
-              top-[43%]
-              w-[145px]
-              sm:left-[5%]
-              sm:top-[45%]
-              sm:w-[195px]
-              md:left-[6%]
-              md:w-[235px]
-              lg:w-[270px]
-              rotate-[-5deg]
-              z-40
-            "
-          >
-            <img
-              src={photos[1]}
-              alt="Foto 2"
-              className="
-                w-full
-                h-[180px]
-                sm:h-[245px]
-                md:h-[295px]
-                lg:h-[330px]
-                object-cover
-              "
-              draggable={false}
-            />
-          </div>
-
-
-          {/* =========================================
-              FOTO 3
-              FOTO SEDANG DI KANAN TENGAH
-          ========================================= */}
-
-          <div
-            className="
-              absolute
-              right-[4%]
-              top-[38%]
-              w-[150px]
-              sm:right-[5%]
-              sm:top-[39%]
-              sm:w-[200px]
-              md:right-[6%]
-              md:w-[245px]
-              lg:w-[280px]
-              rotate-[5deg]
-              z-40
-            "
-          >
-            <img
-              src={photos[2]}
-              alt="Foto 3"
-              className="
-                w-full
-                h-[185px]
-                sm:h-[250px]
-                md:h-[300px]
-                lg:h-[335px]
-                object-cover
-              "
-              draggable={false}
-            />
-          </div>
-
-
-          {/* =========================================
-              FOTO 4
-              FOTO DI BAWAH FOTO KANAN
-          ========================================= */}
-
-          <div
-            className="
-              absolute
-              right-[8%]
-              bottom-[4%]
-              w-[130px]
-              sm:right-[9%]
-              sm:bottom-[5%]
-              sm:w-[180px]
-              md:right-[10%]
-              md:w-[220px]
-              lg:w-[250px]
-              rotate-[-7deg]
-              z-50
-            "
-          >
-            <img
-              src={photos[3]}
-              alt="Foto 4"
-              className="
-                w-full
-                h-[155px]
-                sm:h-[215px]
-                md:h-[260px]
-                lg:h-[295px]
-                object-cover
+                h-auto
+                object-contain
               "
               draggable={false}
             />
@@ -169,36 +61,31 @@ export default function Gallery() {
 
           {/* =========================================
               FOTO 5
-              FOTO BESAR KANAN ATAS
+              KANAN ATAS
+              FOTO BESAR
           ========================================= */}
 
           <div
             className="
               absolute
-              right-[6%]
-              top-[2%]
-              w-[180px]
-              sm:right-[7%]
-              sm:top-[3%]
-              sm:w-[250px]
-              md:right-[8%]
-              md:w-[310px]
-              lg:right-[9%]
-              lg:w-[350px]
-              rotate-[3deg]
               z-10
+
+              left-[57%]
+              top-[13.5%]
+
+              w-[36%]
+
+              rotate-[3deg]
             "
           >
             <img
               src={photos[4]}
               alt="Foto 5"
               className="
+                block
                 w-full
-                h-[240px]
-                sm:h-[330px]
-                md:h-[410px]
-                lg:h-[460px]
-                object-cover
+                h-auto
+                object-contain
               "
               draggable={false}
             />
@@ -206,22 +93,123 @@ export default function Gallery() {
 
 
           {/* =========================================
-              TITLE
+              FOTO 3
+              TENGAH
+              MENUTUPI FOTO 1 DAN FOTO 5
+          ========================================= */}
+
+          <div
+            className="
+              absolute
+              z-50
+
+              left-[36%]
+              top-[28%]
+
+              w-[31%]
+
+              rotate-[7deg]
+            "
+          >
+            <img
+              src={photos[2]}
+              alt="Foto 3"
+              className="
+                block
+                w-full
+                h-auto
+                object-contain
+              "
+              draggable={false}
+            />
+          </div>
+
+
+          {/* =========================================
+              FOTO 2
+              KIRI BAWAH
+              ROTASI MIRING
+          ========================================= */}
+
+          <div
+            className="
+              absolute
+              z-40
+
+              left-[1.5%]
+              top-[49%]
+
+              w-[39%]
+
+              rotate-[-35deg]
+            "
+          >
+            <img
+              src={photos[1]}
+              alt="Foto 2"
+              className="
+                block
+                w-full
+                h-auto
+                object-contain
+              "
+              draggable={false}
+            />
+          </div>
+
+
+          {/* =========================================
+              FOTO 4
+              KANAN BAWAH
+          ========================================= */}
+
+          <div
+            className="
+              absolute
+              z-40
+
+              left-[45%]
+              top-[64%]
+
+              w-[29%]
+
+              rotate-[8deg]
+            "
+          >
+            <img
+              src={photos[3]}
+              alt="Foto 4"
+              className="
+                block
+                w-full
+                h-auto
+                object-contain
+              "
+              draggable={false}
+            />
+          </div>
+
+
+          {/* =========================================
+              JUDUL GALLERY
           ========================================= */}
 
           <h2
             className="
               absolute
-              right-[12%]
-              top-[48%]
               z-[60]
-              text-4xl
-              sm:right-[13%]
-              sm:top-[47%]
-              sm:text-5xl
-              md:right-[15%]
-              md:text-6xl
+
+              left-[66%]
+              top-[54%]
+
+              text-[7vw]
+              sm:text-[5vw]
+              md:text-[4.5vw]
+              lg:text-[4vw]
+
+              leading-none
               text-black
+              whitespace-nowrap
             "
             style={{
               fontFamily: "GreatVibes",
@@ -230,11 +218,9 @@ export default function Gallery() {
             Gallery
           </h2>
 
-
         </div>
 
       </div>
-
     </section>
   );
 }
